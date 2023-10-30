6d3fcbdca757ecb283ffb25d8d748638a23cccf5
stillxthahn <lxthanh235@gmail.com>
2023-10-30 20:57:11 +0700

    ERROR

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -2,11 +2,11 @@ import { useCallback, useState } from "react";
 
 import quizCompleteImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../question.js";
-import QuestionTimer from "./QuestionTimer";
+import Question from "./Question.jsx";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionIndex = userAnswers.length;
 
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = useCallback(function handleSelectAnswer(
@@ -32,29 +32,13 @@ export default function Quiz() {
     );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          //QuestionTimer khong recreated moi khi Quiz render --> tricky: them key
-          key={activeQuestionIndex}
-          timeout={10000}
-          //handleSelectAnswer luon khoi tao lai memory -> useEffect bi execuate lien tuc --> useCallback
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Question
+        index={activeQuestionIndex}
+        onSelectAnswer={handleSelectAnswer}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
